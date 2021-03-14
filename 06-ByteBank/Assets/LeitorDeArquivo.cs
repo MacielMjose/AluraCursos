bfd7d2c855924bcc71f747774c02b6ae406fef36
josé maciel <jlmm92.maciel@gmail.com>
2021-03-14 01:30:05 -0300

    implementação LeitordeArquivo.cs

---
+++ 06-ByteBank/Assets/LeitorDeArquivo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _06_ByteBank.Assets
+{
+    public class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get;}
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+            Console.WriteLine("Abrindo Arquivo:" + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            try
+            {
+                Console.WriteLine("Lendo linha...");
+                throw new FileNotFoundException();
+
+                return "Linha do Arquivo";
+            }
+            catch (IOException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando o Arquivo");
+        }
+    }
+}
--- 06-ByteBank/Program.cs
+++ 06-ByteBank/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _06_ByteBank.Assets;
+using System;
+using System.IO;
 
 namespace _06_ByteBank
 {
@@ -8,12 +10,53 @@ namespace _06_ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cath no método main");
+            }
+        }
+
+        private static void CarregarContas()
+        {
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                //Idisposable to be implemented
+                leitor.LerProximaLinha();
+            }
+
+            //LeitorDeArquivo leitor = null;
+
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas1.txt");
+            //    leitor.LerProximaLinha();
+            //}
+            //catch (IOException)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada");
+            //}
+            //finally //finally é executado sempre
+            //{
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
                 Cliente cliente1 = new Cliente("031.229.220.10", "josé");
                 Cliente cliente2 = new Cliente("031.229.220.10", "ana");
                 ContaCorrente conta1 = new ContaCorrente(12, 12, cliente1);
                 ContaCorrente conta2 = new ContaCorrente(12, 12, cliente2);
-             
-                conta1.Transferir(10000, conta2);
+
+                //conta1.Transferir(10000, conta2);
+                conta1.Sacar(10000);
 
                 Console.ReadLine();
             }
@@ -22,16 +65,20 @@ namespace _06_ByteBank
                 Console.WriteLine($"Não é possível divisão por zero {erro.Message}");
                 //Console.WriteLine(erro.StackTrace);
             }
-            catch(SaldoInsuficienteException ex)
+            catch (SaldoInsuficienteException ex)
             {
                 Console.WriteLine(ex.ValorSaque);
                 Console.WriteLine(ex.Saldo);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
+
+                Console.WriteLine("mensagem da innerException.");
+                Console.WriteLine(ex.InnerException.Message);
+                Console.WriteLine(ex.InnerException.StackTrace);
             }
             catch (ArgumentException erro)
             {
-                if(erro.ParamName == "numero")
+                if (erro.ParamName == "numero")
                 {
                     Console.WriteLine("erro de numero.");
                 }
